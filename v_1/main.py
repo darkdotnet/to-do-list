95fee33d3959bfb75b5a31aae5d4b01ad3419738
darkdotnet <daemonvibes@gmail.com>
2025-06-08 18:54:11 +0200

    nothing

--- v_1/main.py
+++ v_1/main.py
@@ -1,3 +1,17 @@
-print("Hello World!")
+#imports
+from colorama import Fore
 
-##needs to be first line always, otherwise project bug. jk..
+#functions
+
+def menu():
+    print(f"---------------------------------")
+    print(f"{Fore.BLUE}TO-DOs{Fore.RESET} management system")
+    print(f"[{Fore.YELLOW}1{Fore.RESET}] View to-do")
+    print(f"[{Fore.RED}2{Fore.RESET}] Edit to-do")
+    print(f"[{Fore.BLUE}3{Fore.RESET}] Delete to-do")
+    print(f"[{Fore.YELLOW}a{Fore.RESET}] Add to-do")
+    print(f"[{Fore.RED}c{Fore.RESET}] Complete to-do")
+    print(f"---------------------------------")
+    int(input("Please select an option!: "))
+
+menu()
